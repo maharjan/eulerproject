044386e6226505538ddd36d036c117f93506759d
pj <contactprajin@hotmail.com>
2013-09-26 09:00:16 +0545

    problem 11 solved

---
+++ src/main/java/com/pj/eulerproject/Problem12.java
@@ -0,0 +1,13 @@
+package com.pj.eulerproject;
+
+public class Problem12 {
+	
+	public void solve() {
+
+	}
+
+	public static void main(String[] args) {
+		Problem12 twelve = new Problem12();
+		twelve.solve();
+	}
+}
